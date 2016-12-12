4538eae12757c5126a46b1c2041fc4b6d4d0e4b4
Al <needoriginalname@gmail.com>
2016-12-12 16:53:29 -0500

    Added Alarm Controller

---
+++ controllers/alarmcontroller.py
@@ -0,0 +1,89 @@
+import simpleaudio
+import datetime
+import os
+
+
+class AlarmController:
+    def __init__(self, config):
+        # sets the alarm time
+        self.alarm_time = None
+        self.alarm_config = config['Alarm']
+        self.config = config['General']
+        self.set_alarm_time(int(self.alarm_config['hour']), int(self.alarm_config['minute']))
+        self.snooze_time = int(self.alarm_config['snooze'])
+
+        # setup the alarm sound
+        sound_filepath = os.path.join(os.path.dirname(__file__), 'alarm.wav')
+        self.sound_file = simpleaudio.WaveObject.from_wave_file(sound_filepath)
+        self._is_alarm_enabled = self.alarm_config['alarm-enabled']
+        self._is_alarm_on = False
+        self.audio_playing = None
+
+    def snooze(self):
+        time_now = datetime.datetime.now().replace(second=0, microsecond=0)
+        time_snooze = time_now + datetime.timedelta(minutes=self.snooze_time)
+
+        self.set_alarm_time(time_snooze.hour, time_snooze.minute)
+
+    def add_hour_to_alarm_config(self, reverse = False):
+        alarm_hour = int(self.alarm_config['hour'])
+        alarm_min = int(self.alarm_config['minute'])
+        d = datetime.datetime.now().replace(hour=alarm_hour, minute=alarm_min,second=0,microsecond=0)
+        hoursToChange = 1
+        if reverse:
+            hoursToChange = -1
+        d = d + datetime.timedelta(hours=hoursToChange)
+        self.alarm_config['hour'] = str(d.hour)
+        self.alarm_config['minute'] = str(d.minute)
+        self.set_alarm_time(d.hour, d.minute)
+
+    def add_minute_to_alarm_config(self, reverse=False):
+        alarm_hour = int(self.alarm_config['hour'])
+        alarm_min = int(self.alarm_config['minute'])
+        d = datetime.datetime.now().replace(hour=alarm_hour, minute=alarm_min, second=0, microsecond=0)
+        minutessToChange = 1
+        if reverse:
+            minutessToChange = -1
+        d = d + datetime.timedelta(minutes=minutessToChange)
+        self.alarm_config['hour'] = str(d.hour)
+        self.alarm_config['minute'] = str(d.minute)
+        self.set_alarm_time(d.hour, d.minute)
+
+    def set_alarm_time(self, hour, minute):
+        self._is_alarm_on = False
+        alarm_hour = int(hour)
+        alarm_min = int(minute)
+        d = datetime.datetime.now()
+        alarm_today = datetime.datetime.combine(d.date(), d.time().replace(alarm_hour, alarm_min, 0, 0))
+        alarm_tomorrow = datetime.datetime.combine(d.date() + datetime.timedelta(days=1),
+                                                   d.time().replace(alarm_hour, alarm_min, 0, 0))
+
+        if datetime.datetime.now() >= alarm_today:
+            self.alarm_time = alarm_tomorrow
+        else:
+            self.alarm_time = alarm_today
+
+    def update(self):
+        current_time = datetime.datetime.now()
+        if current_time > self.alarm_time:
+            if self._is_alarm_enabled:
+                self._is_alarm_on = True
+            else:
+                # resets alarm for the next day
+                self.set_alarm_time(self.alarm_config['hour'], self.alarm_config['min'])
+
+        if self._is_alarm_on and (self.audio_playing is None or (not self.audio_playing.is_playing())):
+            self.audio_playing = self.sound_file.play()
+        else:
+            if self.audio_playing is not None and self.audio_playing.is_playing():
+                self.audio_playing.stop()
+
+    @staticmethod
+    def _wrap(n, min_value, max_value):
+        if n > max_value:
+            n = (n - max_value) + min_value
+
+        if n < min_value:
+            n = max_value - (min_value - n)
+
+        return n
---
+++ controllers/test_alarmController.py
@@ -0,0 +1,107 @@
+from unittest import TestCase
+import datetime
+import time
+from controllers.alarmcontroller import AlarmController
+
+
+class TestAlarmController(TestCase):
+    def setUp(self):
+        alarmtime = datetime.datetime.now() + datetime.timedelta(minutes=1)
+        h = str(alarmtime.hour)
+        m = str(alarmtime.minute)
+
+        config = {'General': {
+            "time-format": "%I:%M",
+            "date-format": "%m %d"
+        }, "Alarm": {
+            "hour": h,
+            "minute": m,
+            "snooze": str(1),
+            'alarm-enabled': str(True)
+        }}
+
+        config2 = {'General': {
+            "time-format": "%I:%M",
+            "date-format": "%m %d"
+        }, "Alarm": {
+            "hour": 0,
+            "minute": 0,
+            "snooze": str(1),
+            'alarm-enabled': str(True)
+        }}
+
+        self.controller = AlarmController(config)
+        self.controller_midnight_alarm = AlarmController(config2)
+
+    def test_snooze(self):
+        self.controller.alarm_time = datetime.datetime.now()
+
+        time1 = self.controller.alarm_time
+        self.controller.snooze()
+        time2 = self.controller.alarm_time
+        self.assertTrue(time2 > time1)
+
+    def test_add_hour_to_alarm(self):
+        time1 = self.controller.alarm_time
+        self.controller.add_hour_to_alarm_config()
+        time2 = self.controller.alarm_time
+
+        time3 = self.controller.alarm_time
+        self.controller.add_hour_to_alarm_config(True)
+        time4 = self.controller.alarm_time
+
+        self.assertEqual((time2 - time1).seconds, 60 * 60)
+        self.assertEqual((time3 - time4).seconds, 60 * 60)
+
+    def test_alarm_hour_roll_over(self):
+        self.controller_midnight_alarm.set_alarm_time(0,0)
+        t = self.controller_midnight_alarm.alarm_time
+        self.controller_midnight_alarm.add_hour_to_alarm_config()
+        t2 = self.controller_midnight_alarm.alarm_time
+        self.controller_midnight_alarm.add_hour_to_alarm_config(True)
+        t4 = self.controller_midnight_alarm.alarm_time
+
+        self.assertEqual((t2-t).seconds, 60*60)
+        self.assertEqual((t2-t4).seconds, 60*60)
+
+
+    def test_alarm_minute_roll_over(self):
+        self.controller_midnight_alarm.set_alarm_time(0,0)
+        t = self.controller_midnight_alarm.alarm_time
+        self.controller_midnight_alarm.add_minute_to_alarm_config()
+        t2 = self.controller_midnight_alarm.alarm_time
+        self.controller_midnight_alarm.add_minute_to_alarm_config(True)
+        t4 = self.controller_midnight_alarm.alarm_time
+
+        self.assertEqual((t2-t).seconds, 60)
+        self.assertEqual((t2-t4).seconds, 60)
+
+    def test_add_minute_to_alarm(self):
+        time1 = self.controller.alarm_time
+        self.controller.add_minute_to_alarm_config()
+        time2 = self.controller.alarm_time
+
+        time3 = self.controller.alarm_time
+        self.controller.add_minute_to_alarm_config(True)
+        time4 = self.controller.alarm_time
+
+        self.assertEqual((time2 - time1).seconds, 60)
+        self.assertEqual((time3 - time4).seconds, 60)
+
+
+    def test_set_alarm_time(self):
+        time1 = self.controller.alarm_time
+        d = time1 + datetime.timedelta(minutes=1)
+        self.controller.set_alarm_time(d.hour, d.minute)
+        time2 = self.controller.alarm_time
+        self.assertTrue((time2-time1).seconds, 60)
+
+
+    def test_alarm(self):
+        self.controller.update()
+        time.sleep(90)
+        self.controller.update()
+        self.assertTrue(self.controller._is_alarm_on)
+        time.sleep(2)
+        self.controller.snooze()
+        self.assertFalse(self.controller._is_alarm_on)
